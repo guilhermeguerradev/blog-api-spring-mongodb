b0e5fef4f6a7043b542921e04291abf7bed6c83b
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2025-09-18 22:21:37 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/guilhermeguerra/blog_api_spring_mongodb/BlogApiSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeguerra.blog_api_spring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeguerra/blog_api_spring_mongodb/BlogApiSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeguerra.blog_api_spring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
